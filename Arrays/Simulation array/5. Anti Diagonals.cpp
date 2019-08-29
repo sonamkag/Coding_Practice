05b643ab95973f24bf661760c93df29c371badb5
sonamkag <sonamkag@gmail.com>
2019-08-29 13:15:25 +0530

    Update 5. Anti Diagonals.cpp

--- Arrays/Simulation array/5. Anti Diagonals.cpp
+++ Arrays/Simulation array/5. Anti Diagonals.cpp
@@ -1,3 +1,23 @@
+/*
+Give a N*N square matrix, return an array of its anti-diagonals. Look at the example for more details.
+
+Example:		
+Input: 	
+1 2 3
+4 5 6
+7 8 9
+
+Return the following :
+[ [1], [2, 4], [3, 5, 7], [6, 8], [9] ]
+
+Input : 
+1 2
+3 4
+
+Return the following  : 
+[ [1], [2, 3], [4] ]
+*/
+
 vector<vector<int> > Solution::diagonal(vector<vector<int> > &A) {
     int n=A.size(),count=0;
     vector<vector<int>> B(2*n-1);
