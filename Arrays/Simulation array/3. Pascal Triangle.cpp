db4aaaa89cce50ac12b2df4e1333ec2d0a09df97
sonamkag <sonamkag@gmail.com>
2019-08-29 13:11:15 +0530

    Update 3. Pascal Triangle.cpp

--- Arrays/Simulation array/3. Pascal Triangle.cpp
+++ Arrays/Simulation array/3. Pascal Triangle.cpp
@@ -1,3 +1,19 @@
+/*
+Given numRows, generate the first numRows of Pascal’s triangle.
+Pascal’s triangle : To generate A[C] in row R, sum up A’[C] and A’[C-1] from previous row R - 1.
+
+Example:
+Given numRows = 5,
+Return:
+[
+     [1],
+     [1,1],
+     [1,2,1],
+     [1,3,3,1],
+     [1,4,6,4,1]
+]
+*/
+
 vector<vector<int> > Solution::solve(int A) {
     if(A<0){ vector<vector<int>> B; return B; }
      vector<vector<int>> B(A); // #rows = A, #columns varies.
