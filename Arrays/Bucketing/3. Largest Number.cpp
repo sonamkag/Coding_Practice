5d4e7e7b19e49133ba571cde89dfbfeb99a74dde
sonamkag <sonamkag@gmail.com>
2019-08-29 13:31:22 +0530

    Update 3. Largest Number.cpp

--- Arrays/Bucketing/3. Largest Number.cpp
+++ Arrays/Bucketing/3. Largest Number.cpp
@@ -1,3 +1,11 @@
+/*
+Given a list of non negative integers, arrange them such that they form the largest number.
+
+Example:
+Given [3, 30, 34, 5, 9], the largest formed number is 9534330.
+Note: The result may be very large, so you need to return a string instead of an integer.
+*/
+
 int stcompare(string a, string b){
     string ab=a.append(b);
     string ba=b.append(a);
